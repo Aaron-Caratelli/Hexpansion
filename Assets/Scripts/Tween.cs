7eaf4c960cb55338a6283f60610e286470ea4f13
Matt Noone <matt.noone95@gmail.com>
2017-12-03 18:45:34 +1100

    Finish core hex grid + add hex grid controller to select tiles + NEW: Tweening

---
+++ Assets/Scripts/Tween.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hexpansion
+{
+    public class Tween : MonoBehaviour
+    {
+        private bool _isTweening;
+        private Vector3 _destination;
+        public float speed;
+
+        public void TweenTo(Vector3 pos, float speed)
+        {
+            _isTweening = true;
+            _destination = pos;
+            this.speed = speed;
+        }
+
+        public void TweenBy(Vector3 delta, float speed)
+        {
+            _isTweening = true;
+            _destination = transform.position + delta;
+            this.speed = speed;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (_isTweening)
+            {
+                var newPos = Vector3.MoveTowards(this.transform.position, _destination, speed);
+                transform.position = newPos;
+            }                
+        }
+    }
+}
+
